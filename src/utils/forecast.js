d3c5fbcab670e5755884636e5f5bfb56a5078d1e
Vartika Agrawal <20vartika@gmail.com>
2020-09-19 22:15:13 +0530

    Add humidity to forecast

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -20,7 +20,9 @@ const forecast = (latitude, longitude, callback) => {
           body.current.temperature +
           " degrees and it feels like " +
           body.current.feelslike +
-          " degrees out."
+          " degrees out. The humidity is " +
+          body.current.humidity +
+          "% ."
       );
     }
   });
