c299b80501db28f4918005b54bdd1f859919acc7
Vartika Agrawal <20vartika@gmail.com>
2020-09-19 17:05:42 +0530

    Remove unnecessary console.log call

--- public/js/app.js
+++ public/js/app.js
@@ -1,5 +1,3 @@
-console.log("It is loaded");
-
 const weatherForm = document.querySelector("form");
 const search = document.querySelector("input");
 const messageOne = document.querySelector("#message-1");
